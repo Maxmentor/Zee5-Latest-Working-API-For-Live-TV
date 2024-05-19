859ef38519aaff4bd23d9837aca7d2a34e5ea9f0
Max Mentor <104275584+Maxmentor@users.noreply.github.com>
2024-05-19 13:31:05 +0530

    Update zee5.php

--- zee5.php
+++ zee5.php
@@ -3,7 +3,7 @@
 $curl = curl_init();
 $channel =$_GET['c'];
 
-$url="https://spapi.zee5.com/singlePlayback/getDetails/secure?channel_id=$channel";
+$url="https://spapi.zee5.com/singlePlayback/getDetails/secure?channel_id=$channel&device_id=ff6a6d41-28fb-49c2-917f-0f51d5521835&platform_name=mobile_web&translation=en&user_language=hi&country=IN&state=UP&app_version=4.5.1&user_type=premium&check_parental_control=false&gender=Male&age_group=25-32&uid=378f3a58-4284-4a38-9d2d-f6769c0db4aa&ppid=ff6a6d41-28fb-49c2-917f-0f51d5521835&version=12";
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => $url,
