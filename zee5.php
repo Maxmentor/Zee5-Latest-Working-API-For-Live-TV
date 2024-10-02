574f3460caf660c0db481498d84978f22df8bde4
Coder <89525795+DebugDyno@users.noreply.github.com>
2024-10-02 12:17:59 +0530

    fix: account banned probleam

--- zee5.php
+++ zee5.php
@@ -1,41 +1,57 @@
 <?php
 
+/*  Fill this with your actual access token */
+$x_access_token = "";
+$authorization_token = "";
+
+if (isset($_GET["id"])) {
+    $channel = $_GET["id"];
+} else {
+    exit("Error: Channel ID not found.");
+}
+
 $curl = curl_init();
-$channel =$_GET['c'];
 
-$url="https://spapi.zee5.com/singlePlayback/getDetails/secure?channel_id=$channel&device_id=ff6a6d41-28fb-49c2-917f-0f51d5521835&platform_name=mobile_web&translation=en&user_language=hi&country=IN&state=UP&app_version=4.5.1&user_type=premium&check_parental_control=false&gender=Male&age_group=25-32&uid=378f3a58-4284-4a38-9d2d-f6769c0db4aa&ppid=ff6a6d41-28fb-49c2-917f-0f51d5521835&version=12";
+$url = "https://spapi.zee5.com/singlePlayback/getDetails/secure?channel_id=$channel&&device_id=&platform_name=desktop_web&country=IN&check_parental_control=false";
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $url,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS =>'{
-  "x-access-token": "",  
-  "Authorization": ""
-}',
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/json'
-  ),
-));
-
+curl_setopt_array($curl, [
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "application/json",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => '{
+        "x-access-token": "' . $x_access_token. '",
+        "Authorization": "' . $authorization_token . '"
+    }',
+    CURLOPT_HTTPHEADER => [
+        "Content-Type: application/json"
+    ],
+]);
 
 $response = curl_exec($curl);
+
+if ($response === false) {
+    exit("cURL Error: " . curl_error($curl));
+}
+
 curl_close($curl);
 
 $zx = json_decode($response, true);
-$image= $zx["assetDetails"]['image_url'];
-$img = str_replace('270x152', '1170x658', $image);
-$title= $zx["assetDetails"]['title'];
-$des= $zx["assetDetails"]['description'];
-$playit= $zx["keyOsDetails"]['video_token'];
 
+if (json_last_error() !== JSON_ERROR_NONE) {
+    exit("JSON Decode Error: " . json_last_error_msg());
+}
 
+if (isset($zx["keyOsDetails"]) && isset($zx["keyOsDetails"]["video_token"])) {
+    $playit = $zx["keyOsDetails"]["video_token"];
 
-header("Location: $playit");
+    header("Location: $playit");
+} else {
+    exit("Error: Api Response Error.");
+}
 
 ?>
